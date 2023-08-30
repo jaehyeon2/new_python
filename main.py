acd3d4838518d5cb228537863c306d056a9ff14e
jaehyeon2 <wol5971@gmail.com>
2023-08-31 00:14:50 +0900

    fix main.py

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ import os
 import pandas as pd
 import time
 import config
-
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages=[{"role": "user", "content": prompt}]
-    response=openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,
-    )
-    return response.choices[0].message["content"]
+import crowling_function
+import setting_gpt
 
 if __name__ == '__main__':
-    exit()
+    # exit()
     # print("test")
+    keywords=set()
+    keywords.add("파이썬")
+    keywords.add("삼성")
+
+    urls=[]
+    for keyword in keywords:
+        urls.extend([keyword, crowling_function.collect_urls(keyword)])
+
+
+    print(urls)
+
+
 
     # openai.api_key=config.api_key
     #
-    #
-    # prompt = ""
-    # response=get_completion(prompt)
-    #
-    #
+    # prompt = "Hello"
+    # response = setting_gpt.get_completion(prompt)
     #
     # print(response)
+
+    exit(0)
